61bdd113d666ecf8e0ae78fccd728b70982d339e
luosiwei <aiaisl@outlook.com>
2016-02-25 11:07:42 +0800

    creating tasks

--- todo-ionic/www/js/app.js
+++ todo-ionic/www/js/app.js
@@ -4,3 +4,29 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 angular.module('todo', ['ionic'])
+.controller('MainController', function($scope, $ionicModal){
+    $scope.tasks = [];
+    
+    $ionicModal.fromTemplateUrl('new-task.html', function(modal) {
+        $scope.taskModal = modal;
+    }, {
+        scope: $scope,
+        animation: 'slide-in-up'
+    });
+    
+    $scope.createTask = function(task) {
+        $scope.tasks.push({
+            title: task.title
+        });
+        $scope.taskModal.hide();
+        task.title = "";
+    }
+    
+    $scope.newTask = function(){
+        $scope.taskModal.show();
+    }
+    
+    $scope.closeNewTask = function() {
+        $scope.taskModal.hide();
+    }
+})
